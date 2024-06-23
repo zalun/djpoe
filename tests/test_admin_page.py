a45c1ab8fcbda4affd21e24e31e2f417273ca122
Piotr Zalewa <zaloon@gmail.com>
2024-06-23 14:29:32 +0200

    Part 1

---
+++ tests/test_admin_page.py
@@ -0,0 +1,41 @@
+# tests/test_admin_page.py
+import pytest
+from django.contrib.auth.models import User
+from django.test import Client
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_render_admin_login(client: Client):
+    url = reverse("admin:login")
+
+    response = client.get(url)
+
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_admin_redirect_if_not_superuseer(client: Client, django_user_model: User):
+    user = django_user_model.objects.create_user("john", password="pass1234")
+    client.force_login(user)
+    url = reverse("admin:index")
+
+    response = client.get(url)
+
+    assert response.status_code == 302
+    assert response.content == b""
+    assert response.url == "/admin/login/?next=/admin/"
+
+
+@pytest.mark.django_db
+def test_admin_success_for_superuser(client: Client, django_user_model: User):
+    user = django_user_model.objects.create_user(
+        "john", password="pass1234", is_superuser=True, is_staff=True
+    )
+    client.force_login(user)
+    url = reverse("admin:index")
+
+    response = client.get(url)
+
+    assert response.status_code == 200
+    assert b"Django site admin" in response.content
---
+++ tests/test_auth_models_user.py
@@ -0,0 +1,15 @@
+# tests/test_auth_models_user.py
+import pytest
+from django.contrib.auth.models import User
+
+
+@pytest.mark.django_db
+def test_user_create(django_user_model: User):
+    django_user_model.objects.create_user("john", password="pass1234")
+
+    assert django_user_model.objects.count() == 1
+    user = django_user_model.objects.first()
+    assert user is not None
+    assert user.username == "john"
+    assert user.is_active is True
+    assert user.is_staff is False
