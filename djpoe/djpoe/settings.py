8c6f95d2bb5d2a25c652c5088fce811f33d82cc6
Piotr Zalewa <zaloon@gmail.com>
2024-08-04 14:00:58 +0200

    part-4 staticfiles configuration

--- djpoe/djpoe/settings.py
+++ djpoe/djpoe/settings.py
@@ -115,7 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = Path(BASE_DIR).joinpath("staticfiles")
+STATICFILES_DIRS = (Path(BASE_DIR).joinpath("static"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
