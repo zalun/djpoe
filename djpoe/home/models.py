c69e128bce2a6d30e4cf89fa40d0974e18b43227
Piotr Zalewa <zaloon@gmail.com>
2024-09-29 18:00:42 +0200

    fix: ruff not used import

--- djpoe/home/models.py
+++ djpoe/home/models.py
@@ -3,7 +3,6 @@
 # djpoe/home/models.py
 from typing import ClassVar
 
-from django.db import models
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
 from wagtail.models import Page
